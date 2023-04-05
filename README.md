e8aac2eb3eceda6c9622465ccf76943a5ed6dc11
jake8655 <dominik8655@gmail.com>
2023-04-05 12:15:24 +0200

    NEW IDEA + move to exact coords

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # Tkinter-App
 Random tkinter school project
-## Idea
+## A lot better Idea
+- Impossible game
+- map = maze
+- player = circle (smiley face)
+- have to move around maze 
+- some mazes impossible, have to use "bug"
+- "bug" = when player on edge of screen it gets stuck and u can spawn it at any other position at the edge of the screen
+
+## OLD Idea
 - Collecting trash
 - Move with mouse (or arrow keys if cant with mouse cuz of tkinter)
 - Click on trash with "trash collecting stick" to pick up and click on trash bin/bag to drop off
 - Each trash collected is +1 point
 - Trash spawns from the top (maybe ocean brings it) and stops at shore
 - Whole scene is on the shore
-## Issue
+## OLD Issue
 - Still don't know how to move to exact coords
--- main.py
+++ main.py
@@ -2,18 +2,27 @@ import tkinter
 
 WIDTH = 800
 HEIGHT = 600
+PLAYER_WIDTH = 50
+PLAYER_HEIGHT = 50
 
 canvas = tkinter.Canvas(bg='pink', width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-player = canvas.create_oval(WIDTH//2 - 50, HEIGHT//2 - 50, WIDTH//2 + 50, HEIGHT//2 + 50, fill='lightblue', outline='blue')
+def getDistanceBetweenCoords(coords1: tuple[float, float], coords2: tuple[float, float]):
+    return coords1[0] - coords2[0], coords1[1] - coords2[1]
+
+def move(tag: str, coords: tuple[float, float]):
+    tmp_coords = canvas.coords(tag)
+    target_coords = getDistanceBetweenCoords(coords, tmp_coords)
+    canvas.move(tag, *target_coords)
+
+canvas.create_oval(WIDTH//2 - PLAYER_WIDTH, HEIGHT//2 - PLAYER_WIDTH, WIDTH//2 + PLAYER_HEIGHT, HEIGHT//2 + PLAYER_HEIGHT, fill='lightblue', outline='blue', tag='player')
 
 def motion(event):
-    print(event["x"], event["y"])
-    canvas.move(player, event["x"], event["y"])
+    move('player', (event.x-PLAYER_WIDTH, event.y-PLAYER_HEIGHT))
 
 # Mouse movements
-motion({'x': 100, 'y': 100})
-# canvas.bind('<Motion>', motion)
+# motion({'x': 100, 'y': 100})
+canvas.bind('<Motion>', motion)
 
 tkinter.mainloop()
