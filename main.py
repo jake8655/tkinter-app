4ca23a33f340ca7abdb0b63e8ea537348538e749
jake8655 <dominik8655@gmail.com>
2023-04-26 12:28:34 +0200

    feat: A lot of stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from utils import move
 
 WIDTH = 800
 HEIGHT = 600
@@ -7,21 +8,11 @@ PLAYER_HEIGHT = 50
 
 canvas = tkinter.Canvas(bg='pink', width=WIDTH, height=HEIGHT)
 canvas.pack()
-# Face
-canvas.create_oval(WIDTH//2 - PLAYER_WIDTH, HEIGHT//2 - PLAYER_HEIGHT, WIDTH//2 + PLAYER_WIDTH, HEIGHT//2 + PLAYER_HEIGHT, fill='lightblue', outline='blue', tag='player')
-# Eyes
-canvas.create_oval(WIDTH//2 - (PLAYER_WIDTH//2 - 10), HEIGHT//2 - (PLAYER_HEIGHT//2 + 5), WIDTH//2 + (PLAYER_WIDTH//2 - 5), HEIGHT//2 + (PLAYER_HEIGHT//2 - 5), fill='black', tag='player')
-
-def getDistanceBetweenCoords(coords1: tuple[float, float], coords2: tuple[float, float]):
-    return coords1[0] - coords2[0], coords1[1] - coords2[1]
-
-def move(tag: str, coords: tuple[float, float]):
-    tmp_coords = canvas.coords(tag)
-    target_coords = getDistanceBetweenCoords(coords, tmp_coords)
-    canvas.move(tag, *target_coords)
+img = tkinter.PhotoImage(file="assets/player.png")
+canvas.create_image(WIDTH//2,HEIGHT//2, anchor='center', image=img, tag='player')
 
 def motion(event):
-    move('player', (event.x-PLAYER_WIDTH, event.y-PLAYER_HEIGHT))
+    move('player', (event.x, event.y))
 
 # Move player to mouse pos
 canvas.bind('<Motion>', motion)
