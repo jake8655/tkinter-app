8a0f35d34e7ca0d85c212a11b291d5ce1e19441d
Dominik Toth <dominik8655@gmail.com>
2023-05-15 16:10:06 +0200

    feat: Everything working with the minimax algorithm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from os import error
 import tkinter as tk
-from typing import Union
 import minimax
 
 WIDTH = 800
@@ -14,15 +13,6 @@ canvas = tk.Canvas(bg="pink", width=WIDTH, height=HEIGHT)
 canvas.pack()
 
 game_state: minimax.Board = [[None, None, None], [None, None, None], [None, None, None]]
-
-
-def draw_play_area(width: int, height: int):
-    for i in range(2):
-        # Horizontal lines
-        canvas.create_line(100, 200 + i * height, 100 + 3 * width, 200 + i * height, fill="black", width=1)
-        # Vertical lines
-        canvas.create_line(100 + (i + 1) * width, 50, 100 + (i + 1) * width, 50 + height * 3, fill="black", width=1)
-
 
 width_of_square = 200
 height_of_square = 150
@@ -49,13 +39,21 @@ coords_of_squares = (
 )
 
 
-def draw_x(square: minimax.Square):
+def draw_play_area(width: int, height: int):
+    for i in range(2):
+        # Horizontal lines
+        canvas.create_line(100, 200 + i * height, 100 + 3 * width, 200 + i * height, fill="black", width=1)
+        # Vertical lines
+        canvas.create_line(100 + (i + 1) * width, 50, 100 + (i + 1) * width, 50 + height * 3, fill="black", width=1)
+
+
+def draw_x(square: minimax.Square, color: str):
     canvas.create_line(
         coords_of_squares[square[0]][square[1]][0][0] + 20,
         coords_of_squares[square[0]][square[1]][0][1] + 20,
         coords_of_squares[square[0]][square[1]][1][0] - 20,
         coords_of_squares[square[0]][square[1]][1][1] - 20,
-        fill="blue",
+        fill=color,
         width=1,
     )
     canvas.create_line(
@@ -63,64 +61,62 @@ def draw_x(square: minimax.Square):
         coords_of_squares[square[0]][square[1]][1][1] - 20,
         coords_of_squares[square[0]][square[1]][1][0] - 20,
         coords_of_squares[square[0]][square[1]][0][1] + 20,
-        fill="blue",
+        fill=color,
         width=1,
     )
-    game_state[square[0]][square[1]] = "X"
     canvas.update()
 
 
-def check_empty_squares():
-    for i in range(3):
-        for j in range(3):
-            if game_state[i][j] is None:
-                return True
-    return False
-
-
-def check_win():
-    winner = None
-
-    # Check board for 3 in a row or 3 in a column or 3 diagonal
-    for i in range(3):
-        if game_state[i][0] == game_state[i][1] == game_state[i][2] is not None:
-            winner = game_state[i][0]
-        if game_state[0][i] == game_state[1][i] == game_state[2][i] is not None:
-            winner = game_state[0][i]
-    if game_state[0][0] == game_state[1][1] == game_state[2][2] is not None:
-        winner = game_state[0][0]
-    if game_state[0][2] == game_state[1][1] == game_state[2][0] is not None:
-        winner = game_state[0][2]
-
-    if winner:
-        print(f"{winner} won")
-        canvas.after(1000, win.destroy)
-
-
-def draw_o():
-    if not check_empty_squares():
-        print("Draw")
-        return canvas.after(1000, win.destroy)
-
-    square = minimax.find_best_move(game_state)
-    print(game_state)
-
+def draw_o(square: minimax.Square, color: str):
     canvas.create_oval(
         coords_of_squares[square[0]][square[1]][0][0] + 20,
         coords_of_squares[square[0]][square[1]][0][1] + 20,
         coords_of_squares[square[0]][square[1]][1][0] - 20,
         coords_of_squares[square[0]][square[1]][1][1] - 20,
-        outline="red",
+        outline=color,
         width=1,
     )
-
-    game_state[square[0]][square[1]] = "O"
     canvas.update()
-    check_win()
+
+
+def make_human_move(square: minimax.Square):
+    if minimax.HUMAN == "X":
+        draw_x(square, "blue")
+    else:
+        draw_o(square, "blue")
+
+    minimax.make_move(game_state, square, minimax.HUMAN)
+
+
+def handle_win(winner):
+    if not winner:
+        return False
+
+    if winner == "tie":
+        print("Tie!")
+    else:
+        print(f"{winner} wins!")
+
+    win.after(1000, win.destroy)
+    return True
+
+
+def make_ai_move():
+    square = minimax.find_best_move(game_state)
+    print(game_state)
+
+    if minimax.AI == "X":
+        draw_x(square, "red")
+    else:
+        draw_o(square, "red")
+
+    minimax.make_move(game_state, square, minimax.AI)
+
+    handle_win(minimax.check_win(game_state))
 
 
 def calculate_clicked_square(x: int, y: int) -> minimax.Square:
-    square: Union[None, minimax.Square] = None
+    square: None | minimax.Square = None
     for i in range(3):
         for j in range(3):
             if (
@@ -140,19 +136,28 @@ def check_square_is_taken(square: minimax.Square) -> bool:
     return game_state[square[0]][square[1]] is not None
 
 
-def click(e):
-    square = calculate_clicked_square(e.x, e.y)
+def click(event):
+    square = calculate_clicked_square(event.x, event.y)
 
     if check_square_is_taken(square):
         return print("Square already taken")
 
-    draw_x(square)
-    check_win()
-    canvas.after(1000, draw_o)
+    make_human_move(square)
+
+    if handle_win(minimax.check_win(game_state)):
+        return
+
+    canvas.after(1000, make_ai_move)
+
+
+def after_wait():
+    make_ai_move()
+    canvas.bind("<Button-1>", click)
 
 
 draw_play_area(width_of_square, height_of_square)
-canvas.bind("<Button-1>", click)
+if minimax.AI == "X":
+    canvas.after(1000, after_wait)
 
 # Quit when ESC is pressed
 win.bind("<Escape>", lambda _: win.destroy())
--- minimax.py
+++ minimax.py
@@ -1,8 +1,54 @@
 import random
-from typing import Literal, Union
+from typing import Literal
 
 Board = list[list[Literal[None, "X", "O"]]]
 Square = tuple[int, int]
+
+AI = "O"
+HUMAN = "X"
+SCORES = {
+    "X": -1 if AI == "O" else 1,
+    "O": 1 if AI == "O" else -1,
+    "tie": 0,
+}
+
+
+def check_empty_squares(board: Board):
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] is None:
+                return True
+    return False
+
+
+def check_win(board: Board):
+    winner = None
+
+    # Check board for 3 same in a row, in a column or diagonal
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] is not None:
+            winner = board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] is not None:
+            winner = board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] is not None:
+        winner = board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] is not None:
+        winner = board[0][2]
+
+    if not check_empty_squares(board) and not winner:
+        return "tie"
+
+    return winner
+
+
+def make_move(board: Board, square: Square, player: Literal["X", "O"]) -> Board:
+    board[square[0]][square[1]] = player
+    return board
+
+
+def undo_move(board: Board, square: Square) -> Board:
+    board[square[0]][square[1]] = None
+    return board
 
 
 def get_available_squares(game_state: Board) -> list[Square]:
@@ -15,21 +61,33 @@ def get_available_squares(game_state: Board) -> list[Square]:
     return available_squares
 
 
-def minimax(board: Board) -> int:
-    return 1
+def calculate_score_of_next_move(board: Board, depth: int, isMaximizing: bool):
+    best_score = -1000 if isMaximizing else 1000
+    best_move: Square = random.choice(get_available_squares(board))
+    available_squares = get_available_squares(board)
+
+    for square in available_squares:
+        make_move(board, square, AI if isMaximizing else HUMAN)
+        score = minimax(board, depth + 1, not isMaximizing)
+        undo_move(board, square)
+
+        prev_score = best_score
+        best_score = max(score, best_score) if isMaximizing else min(score, best_score)
+        if prev_score != best_score:
+            best_move = square
+
+    return best_score, best_move
+
+
+def minimax(board: Board, depth: int, isMaximizing: bool):
+    result = check_win(board)
+    if result:
+        return SCORES[result]
+
+    score, _ = calculate_score_of_next_move(board, depth, isMaximizing)
+    return score
 
 
 def find_best_move(board: Board) -> Square:
-    available_squares = get_available_squares(board)
-    best_score = -1000
-    best_move: Union[Square, None] = None
-
-    for square in available_squares:
-        board[square[0]][square[1]] = "O"
-        score = minimax(board)
-        board[square[0]][square[1]] = None
-        if score > best_score:
-            best_score = score
-            best_move = square
-
-    return best_move or random.choice(available_squares)
+    _, best_move = calculate_score_of_next_move(board, 0, True)
+    return best_move
